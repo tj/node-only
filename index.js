c9965ccac55a58e87cda452a4de10594c3509246
Christopher Garvis <cgarvis@gmail.com>
2012-10-15 11:34:02 -0300

    Adds basic support for arrays of objects

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-
 module.exports = function(obj, keys){
   obj = obj || {};
-  if ('string' == typeof keys) keys = keys.split(/ +/);
+  if ('string' === typeof keys) keys = keys.split(/ +/);
+
+  if (Array.isArray(obj)) {
+    return obj.map(function(object) { return only(object, keys) });
+  } else {
+    return only(obj, keys);
+  }
+};
+
+var only = function only(obj, keys) {
   return keys.reduce(function(ret, key){
     ret[key] = obj[key];
     return ret;
